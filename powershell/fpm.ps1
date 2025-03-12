7cafeabd006c66a0d032cbd3de90f619dd792e6b
Amasaki Shinobu <amasaki203@gmail.com>
2025-03-12 22:31:14 +0900

    [update] basic commands.

--- powershell/fpm.ps1
+++ powershell/fpm.ps1
@@ -1,61 +1,68 @@
 $fpmCompletions = {
-	param($wordToComplete, $commandAst, $cursorPosition);
+   param($wordToComplete, $commandAst, $cursorPosition);
 
-	# Subcommands
-	$subcommands = ("build", "clean", "help", "install", "new", "publish", "run", "test")
+   # Subcommands
+   $subcommands = @("build", "clean", "help", "install", "new", "publish", "run", "test")
 
-
-	# Long Options
-	$options = @("--version", "--help", "--verbose", "--runner", "--compiler")
-	
-	# runnnerの候補
-	$runnerCommands = @("cafrun", "mpiexec", "mpirun", "gdb", "varglind")
-
-	# コンパイラ候補
-	$compilerCommands = @("gfortran", "ifort", "ifx", "flang")
-	
-
-	# $tokens = $commandAst.Extent.Text -split '\s+'
-
-	$tokens = $commandAst.CommandElements | ForEach-Object { $_.Value }
-
-	$filteredOptions = $options | Where-Object { $_ -notin $tokens }
-
-	if ($cursorPosition -gt $commandAst.Extent.Text.Length) {
-		$lastToken = $tokens[-1]
-	} elseif ($tokens.Count -ge 2) {
-		$lastToken = $tokens[-2]
-	} else { "" }
-
-	# Write-Host "L25: " $filteredOptions
-	# Write-Host "L26: " $lastToken
-
-	if ($lastToken -eq 'fpm') {
-		$filteredItems = ($subcommands) | Where-Object { $_ -like "$wordToComplete*" }
-	} elseif ($lastToken -eq '--runner') {
-		$filteredItems = $runnerCommands | Where-Object { $_ -like "$wordToComplete*" }
-	} elseif ($lastToken -eq '--compiler') {
-		$filteredItems = $compilerCommands | Where-Object { $_ -like "$wordToComplete*"}
-	} else {
-		$filteredItems = ($filteredOptions) | Where-Object { $_ -like "$wordToComplete*" }
-	}
+   $helpcommands = @("build", "clean", "install", "new", "publish", "run", "test")
 
 
-	# $filteredItems = $subcommands | Where-Object { $_ -like "$wordToComplete*" }
+   # Long Options
+   $options = @("--version", "--help", "--verbose", "--runner", "--compiler")
+   
+   # runnnerの候補
+   $runnerCommands = @("cafrun", "mpiexec", "mpirun", "gdb", "varglind")
 
-	foreach ($item in $filteredItems)
-	{
-		$completionText = $item;
+   # compilers
+   $compilerCommands = @("gfortran", "ifort", "ifx", "flang")
+   
+   $exeFiles = Get-ChildItem -Path ".\build\*\app\*.exe" -File | ForEach-Object { $_.BaseName}
 
-		$listItemText = $item;
 
-		$toolTip = "'$item' subcommand of fpm (Fortran Package Manager) command";
+   $tokens = @($commandAst.CommandElements | ForEach-Object { $_.Value })
+   $lastToken = $tokens[-2]
+   $currToken = $tokens[-1]   
 
-		# $resultType = [System.Management.Automation.CompletionResultType]::ProviderItem;
-		$resultType = [System.Management.Automation.CompletionResultType]::ParameterValue;
+   $filteredOptions = $options | Where-Object { $_ -notin $tokens }
 
-		[System.Management.Automation.CompletionResult]::new($completionText, $listItemText, $resultType, $toolTip);
-	}
+   Write-Host "L26:"
+   Write-Host "L27: count: "$tokens.Count
+   Write-Host "L28: lastToken: '$lastToken'"
+   Write-Host "L29: currToken: '$currToken'"
+   Write-Host "L30: exeFiles : $exeFiles"
+
+   if (($currToken -eq "fpm" -and $tokens.Count -eq 1) -or ($lastToken -eq "fpm" -and $tokens.Count -eq 2)) {
+      $filteredItems = ($subcommands) | Where-Object { $_ -like "$wordToComplete*"}
+
+   } elseif (($currToken -eq 'run') -or ($lastToken -eq 'run') ) {
+      $filteredItems = ($exeFiles) | Where-Object {$_ -like "$wordToComplete*" }
+
+   } elseif ($lastToken -eq '--runner') {
+      $filteredItems = ($runnerCommands) | Where-Object { $_ -like "$wordToComplete*" }
+
+   } elseif ($lastToken -eq '--compiler') {
+      $filteredItems = ($compilerCommands) | Where-Object { $_ -like "$wordToComplete*"}
+
+   } elseif ($lastToken -eq 'help') {
+      $filteredOptions = ($helpcommands) | Where-Object { $_ -like "$wordToComplete*"}
+   } 
+   else {
+      $filteredItems = ($filteredOptions) | Where-Object { $_ -like "$wordToComplete*" }
+   }
+
+   foreach ($item in $filteredItems)
+   {
+      $completionText = "$item ";
+
+      $listItemText = $item;
+
+      $toolTip = "'$item' subcommand of fpm (Fortran Package Manager) command";
+
+      # $resultType = [System.Management.Automation.CompletionResultType]::ProviderItem;
+      $resultType = [System.Management.Automation.CompletionResultType]::ParameterValue;
+
+      [System.Management.Automation.CompletionResult]::new($completionText, $listItemText, $resultType, $toolTip);
+   }
 
 }
 
